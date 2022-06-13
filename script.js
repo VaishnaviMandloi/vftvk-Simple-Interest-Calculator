34eab17c928a5565e7301524bbd71c548b2beabe
VaishnaviMandloi <0808cs201196.ies@ipsacademy.org>
2022-06-13 14:52:44 +0000

    update final

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposite <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
 
     if (principal == "") { //Here Check is empty
         alert("Amount can't by empty or alphabet character");
@@ -20,11 +17,16 @@ function compute()
         }
     }
 
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="\<br\>If you deposite <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+
+
 }
     function updateRate()
     {
         var rateval = document.getElementById("rate").value;
-        document.getElementsById("rate_val").innerText=rateval;
+        document.getElementById("rate_val").innerText=rateval+"%";
     }
 
         
