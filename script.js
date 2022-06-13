7a7b5772180ea434e2e2da5fe78c28683f32a4b2
VaishnaviMandloi <0808cs201196.ies@ipsacademy.org>
2022-06-13 06:56:26 +0000

    upadte js 2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function compute()
     
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
-    var years = getElementById("years").value;
+    var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerHTML="If you deposite "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
